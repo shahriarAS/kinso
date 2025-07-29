6ff0c18e22d96192bf693353cb706e7e31e017fa
shahriarAS <shovonshahriar01@gmail.com>
2025-07-29 18:24:43 +0600

    fix: Adjust layout structure for improved styling consistency in DashboardLayout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -6,9 +6,9 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <section className="bg-primary h-screen w-full text-white p-4 pl-0 flex">
+    <section className="flex w-full h-screen p-4 pl-0 overflow-y-hidden text-white bg-primary">
       <DashboardSidebar />
-      <div className="border flex-1 rounded-xl bg-gray-50 text-primary w-full overflow-x-scroll no-scrollbar">
+      <div className="flex-1 w-full overflow-x-scroll border rounded-xl bg-gray-50 text-primary no-scrollbar">
         {children}
       </div>
     </section>
